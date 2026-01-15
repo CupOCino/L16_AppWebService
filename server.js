04c69462be7e6daa5f53ac5c3c83e191919f0d99
JJ <jiajieloh1112@gmail.com>
2026-01-15 15:07:43 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+const port = 3000;
+
+// database config info
+const dbConfig = {
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    waitForConnections: true,
+    connectionLimit: 100,
+    queueLimit: 0,
+};
+
+// initialize Express app
+const app = express();
+// helps app to read JSON
+app.use(express.json());
+
+// start server
+app.listen(port, () => {
+    console.log('Server running on port', port);
+});
+
+// Example Route: Get all cards
+app.get('/allcards', async (req, res) => {
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        const [rows] = await connection.execute('SELECT * FROM defaultdb.cards');
+        res.json(rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: 'Server error for allcards' });
+    }
+});
+
+// Example Route: Create a new card
+app.post('/addcard', async (req, res) => {
+    const { card_name, card_pic } = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('INSERT INTO cards (card_name, card_pic) VALUES (?, ?)', [card_name, card_pic]);
+        res.status(201).json({message: 'Card ' + card_name + ' added successfully'});
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not add card ' + card_name});
+    }
+});
+
+// Example Route: Update card
+app.post('/updatecard', async (req, res) => {
+    const { id, card_name, card_pic } = req.body;
+    try {
+        let connection = await mysql.createConnection(dbConfig);
+        await connection.execute('UPDATE cards SET card_name = ?, card_pic = ? WHERE id = ?', [card_name, card_pic, id]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({message: 'Server error - could not update card ' + card_name});
+    }
+})
